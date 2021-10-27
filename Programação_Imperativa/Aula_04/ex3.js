23fe0bdf215b62bfbbbc66e52cc80b6d3642efb2
MateusPraxedes <88166393+MateusPraxedes@users.noreply.github.com>
2021-10-26 21:05:42 -0300

    Add files via upload

---
+++ Programação_Imperativa/Aula_04/ex3.js
@@ -0,0 +1,24 @@
+
+// IMC
+
+const nome = "Mateus Praxedes"
+let idade = 25;
+let peso = 90;
+let altura = 1.85;
+let plano = "ouro";
+let imc = peso/(altura * altura);
+
+console.log(nome+" tem "+idade+" anos e seu índice de massa corporal é "+imc);
+
+
+
+
+
+
+
+
+
+
+
+
+
