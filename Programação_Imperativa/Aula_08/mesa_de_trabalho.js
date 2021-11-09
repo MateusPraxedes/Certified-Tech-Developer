3e262941ced5f359f3da94a8157b7bece9d217fb
MateusPraxedes <88166393+MateusPraxedes@users.noreply.github.com>
2021-11-09 20:22:45 -0300

    Add files via upload

--- Programação_Imperativa/Aula_08/mesa_de_trabalho.js
+++ Programação_Imperativa/Aula_08/mesa_de_trabalho.js
@@ -1,13 +1,28 @@
-function podesubir(altura, vemAcompanhado ){
+function podeSubir(altura, vemAcompanhado ){
 let podesubir = false;
 if (altura > 1.40 && altura < 2){
   return  true; 
 }
-else if (altura <= 1.40 && vemAcompanhado){
+else if (altura >= 1.20 && vemAcompanhado){
   return true;
 }
 else{
   return false;
 }}
 
-podesubir(1.40, true);
+console.log(podeSubir(1.15, true));
+
+function podeSubirMeensagem(altura, vemAcompanhado ){
+  if (altura > 1.40 && altura < 2){
+    return  console.log("Acesso autorizado"); 
+  }
+  else if (altura >= 1.20 && vemAcompanhado){
+    return console.log("Acesso autorizado somente com acompanhante"); 
+  }
+  else{
+    return console.log("Acesso negado"); 
+  }}
+
+
+
+podeSubirMeensagem(1.45, false);
