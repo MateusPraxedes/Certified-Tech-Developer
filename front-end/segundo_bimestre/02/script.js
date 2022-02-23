9b37e9dc5bb5e25f0f2d8f7f26a81eb8145916cf
mateuspraxedes <mateuspr97@gmail.com>
2022-02-23 17:05:23 -0300

    mesa de trabalho finalizada

--- front-end/segundo_bimestre/02/script.js
+++ front-end/segundo_bimestre/02/script.js
@@ -1,19 +1,19 @@
-let nomeCompleto = prompt("Digite seu nome completo:")
+let nomeCompleto = prompt("Digite seu nome completo:");
 let telefone;
-let whataapp;
+let whatsapp;
 if ((nomeCompleto == null) || (nomeCompleto == "")) {
-  alert("É obrigatório preencher o nome")
-  nomeCompleto = prompt("Digite seu nome completo:")
+  alert("É obrigatório preencher o nome");
+  nomeCompleto = prompt("Digite seu nome completo:");
   telefone = prompt("Digite o seu telefone") ? (telefone != null) || (telefone != "") : telefone = prompt("Digite o seu telefone");
   alert(`Muito obrigado ${nomeCompleto}, um de nossos consultores entrará em contato contigo através do número ${telefone}.`);
-  whatsapp = confirm("aceita receber mensagem por WhatsApp")
-  console.log(`nome completo: ${nomeCompleto} \ntelefone: ${telefone} \nWhatsapp: ${whatsapp}`)
+  whatsapp = confirm("aceita receber mensagem por WhatsApp");
+  console.log(`nome completo: ${nomeCompleto} \ntelefone: ${telefone} \nWhatsapp: ${whatsapp}`);
 }
 else if ((nomeCompleto != null) || (nomeCompleto == "") && (telefone != null) || (telefone != "")) {
   telefone = prompt("Digite o seu telefone");
   alert(`Muito obrigado ${nomeCompleto}, um de nossos consultores entrará em contato contigo através do número ${telefone}.`);
-  whatsapp = confirm("aceita receber mensagem por WhatsApp")
-  console.log(`nome completo: ${nomeCompleto} \ntelefone: ${telefone} \nWhatsapp: ${whatsapp}`)
+  whatsapp = confirm("aceita receber mensagem por WhatsApp");
+  console.log(`nome completo: ${nomeCompleto} \ntelefone: ${telefone} \nWhatsapp: ${whatsapp}`);
 }
 
 
