c6a37be8680608a13fa13600ab3c23b2f13335ea
mateuspraxedes <mateuspr97@gmail.com>
2022-03-07 20:25:45 -0300

    mesa de trabalho finalizada

---
+++ front-end/segundo_bimestre/04/mesa-de-trabalho/assets/js/main.js
@@ -0,0 +1,15 @@
+let lista = document.querySelectorAll(".lista__item");
+lista[0].classList.add('removido')
+lista[0].innerText = "Carro - Removido"
+
+lista[1].classList.add('selecionado')
+lista[1].innerText = "Bike - Selecionado"
+
+lista[2].classList.add('selecionado')
+lista[2].innerText = "Patinete - Selecionado"
+
+lista[3].classList.add('removido')
+lista[3].innerText = "Celular - Removido"
+
+lista[4].classList.add('removido')
+lista[4].innerText = "Carregador - Removido"
