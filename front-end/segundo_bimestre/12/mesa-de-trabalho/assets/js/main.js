60be0b20147525f5d59d5bacd65634c9e9d1fb75
mateuspraxedes <mateuspr97@gmail.com>
2022-03-24 14:21:36 -0300

    Mesa de trabalho finalizada

--- front-end/segundo_bimestre/12/mesa-de-trabalho/assets/js/main.js
+++ front-end/segundo_bimestre/12/mesa-de-trabalho/assets/js/main.js
@@ -1,71 +1,55 @@
+// 1. Crie um objeto literal para armazenar os itens considerando as propriedades: id, tarefa, autor.
 
-var formulario = document.forms['formularioDeCadastro'];
+// 2. Converta o objeto literal em um json.
 
-formulario.addEventListener('submit', function(evento) {
-
-    var valorDoCampoItem = evento.target['item'].value.trim();
-    
-    // 1. Crie um objeto literal para armazenar os itens considerando as propriedades: id, tarefa, autor.
-    
-    // 2. Converta o objeto literal em um json.
-
-    // 3. Armazene o json no localStorage ou sessionStorage.
-
-});
+// 3. Armazene o json no localStorage ou sessionStorage.
 
 let validacao = localStorage.getItem("Json");
-
-
+let btn = document.createElement('button');
 
 if (validacao === null) {
-    console.log('LocalStorage Vazio')
-   }
-
-
-
-let  objetoLiteral = {
-    id : 1,
-    tarefa: 'Fazer um bolo',  
-    autor:  'Mateus',
-    MetodoData: data = () => {
-        let date = new Date()
-        let dia  = date.getDate();
-        let mes = date.getMonth();
-        let ano = date.getFullYear();  
-        let hora = date.getHours(); 
-        let min = date.getMinutes() ; 
-         
-
-        return `${dia}/${++mes}/${ano},  ${hora}h:${min}min`;
-    } ,
+    console.log("LocalStorage Vazio");
+    alert('LocalStorage Vazio')
+    btn.innerText = 'Ativar'
+    document.body.appendChild(btn)
+    btn.setAttribute("onclick", "ativar()")
+} else{
+    let span = document.createElement("span");
+    let body = document.querySelector("body");
+    span.innerText = localStorage.getItem("Json");
+    body.appendChild(span);
 }
 
+function ativar() {
+  let objetoLiteral = {
+    id: 1,
+    tarefa: "Fazer um bolo",
+    autor: "Mateus",
+    MetodoData: (data = () => {
+      let date = new Date();
+      let dia = date.getDate();
+      let mes = date.getMonth();
+      let ano = date.getFullYear();
+      let hora = date.getHours();
+      let min = date.getMinutes();
 
-objetoLiteral.data = objetoLiteral.MetodoData()
+      return `${dia}/${++mes}/${ano},  ${hora}h:${min}min`;
+    }),
+  };
 
+  objetoLiteral.data = objetoLiteral.MetodoData();
 
-let json = JSON.stringify(objetoLiteral); 
+  let json = JSON.stringify(objetoLiteral);
 
-localStorage.setItem("Json", json );
+  localStorage.setItem("Json", json);
 
-console.log(json)
+  console.log(json);
 
+  let span = document.createElement("span");
+  let body = document.querySelector("body");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  span.innerText = json;
+  body.appendChild(span);
+  body.removeChild(btn);
+  
+}
