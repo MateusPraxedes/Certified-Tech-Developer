e0910c59aefb2933a1e33038e83c511617b8380f
mateuspraxedes <mateuspr97@gmail.com>
2022-04-02 18:40:57 -0300

    Mesa de trabalho finalizada

--- front-end/segundo_bimestre/15/mesa-de-trabalho/assets/js/main.js
+++ front-end/segundo_bimestre/15/mesa-de-trabalho/assets/js/main.js
@@ -1,30 +1,22 @@
 
-var formulario = document.forms['formularioDeCadastro'];
-
-formulario.addEventListener('submit', function(evento) {
-
-    var valorDoCampoItem = evento.target['item'].value.trim();
-    
-    // 1. Crie um objeto literal para armazenar os itens considerando as propriedades: id, tarefa, autor.
-    
-    // 2. Converta o objeto literal em um json.
-
-    // 3. Armazene o json no localStorage ou sessionStorage.
-
-});
-
 let lista = document.querySelector('.c-lista');
-
-
+let ul = document.querySelectorAll('.c-lista__item');
 
 function f() {
-    fetch("https://jsonplaceholder.typicode.com/todos/")
+    fetch("https://jsonplaceholder.typicode.com/todos?userId=1")
     .then(r => {return r.json();
   })
-    .then(r => {
-      console.log(r)
+    .then(tarefas => {
+      lista.removeChild(ul[0])
+      tarefas.forEach(function (tarefa) {
+      let li = document.createElement('li');
+      li.classList.add("c-lista__item");
+      li.innerText = tarefa.title;
+      lista.appendChild(li);
+
+
       })
-    }
+    })}
   
 
     f()
