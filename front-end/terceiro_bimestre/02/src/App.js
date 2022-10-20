b8af94a51cbfdb5aa0f60f18426915e339eed1b9
mateuspraxedes <mateuspr97@gmail.com>
2022-10-20 15:55:03 -0300

    .

---
+++ front-end/terceiro_bimestre/02/src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import "./index.css";
+
+function App() {
+  return ( <>
+<h1>Hello World</h1>
+<p className="title">Meu Conteudo</p>
+<button onClick={() => alert("Ok")}>Pressione</button>
+  </>
+  );
+}
+
+export default App;
---
+++ front-end/terceiro_bimestre/03/src/App.jsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+function App() {
+  return (
+    <>
+      <h1>My app</h1>
+    </>
+  )
+}
+
+
+export default App
+
