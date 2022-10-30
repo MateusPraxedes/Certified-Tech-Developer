83a579696e2a7ff7c0aba0ca7c49df0d233b60c0
Mateus Praxedes <88166393+MateusPraxedes@users.noreply.github.com>
2022-10-29 23:29:22 -0300

    Delete BD directory

--- BD/Aula_09.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE SPOTIFY;
-CREATE TABLE usuario(
-    id_usuario 	INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    nomeusuario	VARCHAR(45),
-    data_nasc	DATE,
-    sexo		VARCHAR(1),
-    email 		VARCHAR(150),
-    senha 		VARCHAR(45)
-);
